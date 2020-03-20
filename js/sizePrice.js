d57d9e28907f5f19b1ae954ae410ba5f610a1cdb
OksanaPos <oxa.poxa@yandex.ru>
2020-03-19 20:07:23 -0700

    последнее

--- js/sizePrice.js
+++ js/sizePrice.js
@@ -1,3 +1,6 @@
-function sizePrice() {
-    size = document.getElementById("size").value;
-    img = document.getElementById("Price");
+var slider=document.getElementsById('myRange');
+var output=document.getElementById('');
+output.=slider.value;
+slider.oninput=function(){
+  output.=this.value;
+}
