8969ec1747c3fb549f369df00b16a19cb55d2dba
OksanaPos <oxa.poxa@yandex.ru>
2020-03-19 21:52:22 -0700

    на проверку

--- js/catalog.js
+++ js/catalog.js
@@ -1,7 +1,5 @@
-
 var popup = document.querySelector(".modal");
 var link = document.querySelector(".popup-link");
-var close=document.querySelector(".close-btn");
 var body = document.body
 var form = popup.querySelector("form");
 if (form) {
@@ -18,7 +16,6 @@ var mapPopup=document.querySelector(".modal-map");
 link.addEventListener("click",function(evt){
   evt.preventDefault();
   body.classList.add("modal-show");
-  console.log("123")
  if (login) {
   login.focus();
  }
@@ -34,35 +31,12 @@ window.onload = function() {
 };
 
 
-close.addEventListener("click",function(evt){
-  evt.preventDefault();
+var elementsClose = document.querySelectorAll(".close-btn");
+console.log(elementsClose);
+for (var i = 0; i < elementsClose.length; i++) {
+  elementsClose[i].onclick = function(){
   body.classList.remove("modal-show");
   body.classList.remove("modal-show-feedback");
   popup.classList.remove("modal-error");
-});
-
-
-console.log(login)
-
-form.addEventListener("submit",function(evt){
-  if(!login.value){
-    evt.preventDefault();
-    popup.classList.remove("modal-error");
-    popup.offsetWidth=popup.offsetWidth;
-    popup.classList.add("modal-error");
-    console.log("Нужно ввести логин");
-  }else {
-    loginStore.innerText = login.value
-    localStorage.setItem("login",login.value);
-    body.classList.remove("modal-show");
+  };
 }
-});
-
-window.addEventListener("Keydown",function(evt){
-  if(evt.keyCode===27){
-    if(popup.classList.contains("modal-show")){
-      evt.preventDefault();
-      popup.classList.remove("modal-show")
-    }
-  }
-});
